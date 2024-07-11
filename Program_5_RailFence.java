a606a2bebc2638874430f0ee31f49893a3605115
Java43-op <muffincrimson@gmail.com>
2024-07-12 05:05:19 +0530

    Update Program_5_RailFence.java

--- Program_5_RailFence.java
+++ Program_5_RailFence.java
@@ -1,13 +1,14 @@
-package Ext;
-
 import java.util.*;
 
 public class Program_5_RailFence {
+
     public static String encrypt(String text, int key) {
         char rail[][] = new char[key][text.length()];
+
         for (int i = 0; i < key; i++) {
             Arrays.fill(rail[i], '\n');
         }
+
         boolean dirDown = false;
         int row = 0, col = 0;
 
@@ -17,12 +18,14 @@ public class Program_5_RailFence {
             }
 
             rail[row][col++] = text.charAt(i);
+
             if (dirDown) {
                 row++;
             } else {
                 row--;
             }
         }
+
         StringBuilder res = new StringBuilder();
         for (int i = 0; i < key; i++) {
             for (int j = 0; j < text.length(); j++) {
@@ -34,13 +37,73 @@ public class Program_5_RailFence {
         return res.toString();
     }
 
+    public static String decrypt(String cipher, int key) {
+        char rail[][] = new char[key][cipher.length()];
+
+        for (int i = 0; i < key; i++) {
+            Arrays.fill(rail[i], '\n');
+        }
+
+        boolean dirDown = false;
+        int row = 0, col = 0;
+
+        for (int i = 0; i < cipher.length(); i++) {
+            if (row == 0 || row == key - 1) {
+                dirDown = !dirDown;
+            }
+
+            rail[row][col++] = '*';
+
+            if (dirDown) {
+                row++;
+            } else {
+                row--;
+            }
+        }
+
+        int index = 0;
+        for (int i = 0; i < key; i++) {
+            for (int j = 0; j < cipher.length(); j++) {
+                if (rail[i][j] == '*' && index < cipher.length()) {
+                    rail[i][j] = cipher.charAt(index++);
+                }
+            }
+        }
+
+        StringBuilder res = new StringBuilder();
+        row = 0;
+        col = 0;
+        for (int i = 0; i < cipher.length(); i++) {
+            if (row == 0 || row == key - 1) {
+                dirDown = !dirDown;
+            }
+
+            if (rail[row][col] != '\n') {
+                res.append(rail[row][col++]);
+            }
+
+            if (dirDown) {
+                row++;
+            } else {
+                row--;
+            }
+        }
+        return res.toString();
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the text to be encrypted: ");
         String text = sc.nextLine();
         System.out.println("Enter the key: ");
         int key = sc.nextInt();
-        System.out.println("Encrypted text: " + encrypt(text, key));
+
+        String encryptedText = encrypt(text, key);
+        System.out.println("Encrypted text: " + encryptedText);
+
+        String decryptedText = decrypt(encryptedText, key);
+        System.out.println("Decrypted text: " + decryptedText);
+
         sc.close();
     }
 }
