14fd7a78ac87b14769041b03012bc180e4fb3940
Java43-op <muffincrimson@gmail.com>
2024-07-12 06:02:28 +0530

    Update rowcol.java

--- rowcol.java
+++ rowcol.java
@@ -3,9 +3,10 @@ public class rowcol {
         int col=4;
         String pt="javaprogrammingx";
         int[] key={2,3,0,1};
-        char[][] mat=new char[pt.length()/col][col];
+        int row = pt.length()/col;
+        char[][] mat=new char[row][col];
         int cnt=0;
-        for(int i=0;i<pt.length()/col;i++){
+        for(int i=0;i<row;i++){
             for(int j=0;j<col;j++){
                 mat[i][j]=pt.charAt(cnt);
                 cnt++;
@@ -15,24 +16,24 @@ public class rowcol {
         int kind=0;
         for(int i;kind<col;kind++){
             i=key[kind];
-            for(int j=0;j<pt.length()/col;j++){
+            for(int j=0;j<row;j++){
                 ct.append(mat[j][i]);
             }
         }
         System.out.println(ct);
         kind=0;
         cnt=0;
-        char[][] newmat=new char[pt.length()/col][col];
+        char[][] newmat=new char[row][col];
         for(int i;kind<col;kind++){
             i=key[kind];
-            for(int j=0;j<pt.length()/col;j++){
+            for(int j=0;j<row;j++){
                 newmat[j][i]=ct.charAt(cnt);
                 cnt++;
             }
         }
         StringBuilder t=new StringBuilder();
         for(int i=0;i<col;i++){
-            for(int j=0;j<pt.length()/col;j++){
+            for(int j=0;j<row;j++){
                 t.append(newmat[i][j]);
             }
         }
